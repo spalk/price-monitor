23f64670764ce62aa8870bd04c2f04b91d595b52
demetrius <d.natxa@gmail.com>
2020-12-28 00:23:27 +0300

    Product model

--- back/price_monitor/api/models.py
+++ back/price_monitor/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.CharField(max_length=500)
+    is_active = models.BooleanField(null=False, default=True)
+    number_of_followers = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 
+
+    def __unicode__(self):
+        return 
